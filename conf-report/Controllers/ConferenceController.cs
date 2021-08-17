d163e81c05f79ae05b8d567f3adf6008852fe62d
Furutek <ku.hexide.su@gmail.com>
2021-08-17 23:19:05 +0900

    Controllers/ConferenceControllers.cs を追加し、必要そうな関数の名前だけ定義しました。

---
+++ conf-report/Controllers/ConferenceController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace conf_report.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        // GET: Conference
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+        public ActionResult EditConference()
+        {
+            return View();
+        }
+
+        public ActionResult EditParticipant()
+        {
+            return View();
+        }
+
+        public ActionResult ConfirmConference()
+        {
+            return View();
+        }
+
+        public ActionResult ConfirmParticipant()
+        {
+            return View();
+        }
+
+        public ActionResult CreateConference()
+        {
+            return View();
+        }
+
+        public ActionResult CreateParticipant()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteConference()
+        {
+            return View();
+        }
+
+        public ActionResult DeleteParticipant()
+        {
+            return View();
+        }
+    }
+}
